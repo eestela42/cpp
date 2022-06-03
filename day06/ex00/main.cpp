afa519aec50f2e7877b4658480750842e8596565
Estela ESTELA <eestela@e2r11p7.clusters.42paris.fr>
2022-06-03 16:25:34 +0200

    wrong main06

--- day06/ex00/main.cpp
+++ day06/ex00/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+void	convChar(std::string &str)
+{
+	char c;
+	if (str.size() == 1)
+		c = static_cast<char>str[0];
+	
+}
+
 int	main(int ac, char **av)
 {
 	if (ac!= 2 || !av[1])
